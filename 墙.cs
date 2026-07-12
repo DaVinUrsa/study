06ad64355c090858c7b5c4376324ed53dd427119
达芬·熊 <2133174758@qq.com>
2026-07-12 22:51:35 +0800

    Add files via upload

---
+++ 墙.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static 贪吃蛇.Number;
+
+namespace 贪吃蛇
+{
+    public static class Wall
+    {
+        
+        public static void print()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i <= x; i++)
+            {
+                Console.Write('0');
+            }
+
+            Console.SetCursorPosition(x, 0);
+            for (int i = 0; i <= y; i++)
+            {
+                Console.SetCursorPosition(x, i);
+                Console.Write('0'); 
+            }
+
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i <= y; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write('0');
+            }
+
+            Console.SetCursorPosition(0, y);
+            for (int i = 0; i <= x; i++)
+            {
+                Console.Write('0');
+            }
+
+        }
+    }
+}
---
+++ 界面的打印.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using 贪吃蛇;
+using static 贪吃蛇.Number;
+
+namespace 贪吃蛇
+{
+    public static class  Iprint
+    {
+        public static int M, N;
+        public static bool C;
+        public static string Str;
+
+        static public void pdraw(int m, int n, bool c, string str)
+        {
+            M = m;
+            N = n;
+            C = c;
+            Str = str;
+            Console.ForegroundColor = C ? ConsoleColor.Green : ConsoleColor.Yellow;
+            Console.SetCursorPosition(M, N);
+            Console.Write(Str);
+        }
+
+    }
+}
